5f51b07b237de0c171547af9b0dc31a288dd7e06
Alrun <fkhey2016@yandex.ru>
2017-05-10 08:18:20 +0300

    10 may

--- projects/mbeg/dist/js/main.js
+++ projects/mbeg/dist/js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 		fade: true,
 		asNavFor: '.slider-main'
 	});
-	$('.slider-work').slick({
+	$('.slider-works').slick({
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		arrows: true,
@@ -41,42 +41,13 @@ $(document).ready(function(){
 		]
 	});
 	$('.slider-partner').slick({
-		slidesToShow: 9,
-		slidesToScroll: 1,
 		arrows: true,
+		initialSlide: 7,
 		dots: false,
 		infinite: true,
-		speed: 300,
-		responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 7,
-				slidesToScroll: 1,
-			}
-		},
-		{
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 5,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 544,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}
-		]
+		variableWidth: true,
+		centerMode: true,
+		speed: 300
 	});
 
 });
@@ -84,7 +55,7 @@ $(document).ready(function(){
   'use strict';
 
   var file = 'img/sprite.symbol.svg',
-      revision = 24;
+      revision = 5;
 
   if ( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
     return true;
@@ -127,3 +98,44 @@ $(document).ready(function(){
   catch(e){}
 
 }( window, document ) );
+
+$(function() {
+  $('#save').click(function() {
+    var formValid = true;
+
+    // $('input, textarea').each(function() {
+    //   var formGroup = $(this).parents('.form-group');
+    //   var glyphicon = formGroup.find('.form-control-feedback');
+
+    //   if (this.checkValidity()) {
+    //     formGroup.addClass('has-success').removeClass('has-error');
+    //     glyphicon.addClass('glyphicon-ok').removeClass('glyphicon-remove');
+    //   } else {
+    //     formGroup.addClass('has-error').removeClass('has-success');
+    //     glyphicon.addClass('glyphicon-remove').removeClass('glyphicon-ok');
+    //     formValid = false;
+    //     }
+    // });
+
+    if (formValid) {
+      $('form').val(function() {
+        var str = $(this).serialize();
+
+        $.ajax({
+          url: "send.php",
+          type: 'POST',
+          data: str,
+          success: function(data) {
+            data
+          }
+        });
+        console.log(str);
+      });
+
+      // $('#myModal').modal('hide');
+      // $('#success-alert').removeClass('hidden');
+      $('#save').attr('disabled', 'disabled');
+
+    }
+  });
+});
