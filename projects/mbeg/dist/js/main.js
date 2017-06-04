f6e617a6fee9a74ebe2df72e8614f7b689543a71
Alrun <fkhey2016@yandex.ru>
2017-06-05 01:09:09 +0300

    Update main.js

--- projects/mbeg/dist/js/main.js
+++ projects/mbeg/dist/js/main.js
@@ -31,7 +31,7 @@ d.slice(e-c+1,e+c+2).addClass("slick-active").attr("aria-hidden","false")),0===a
   'use strict';
 
   var file = 'img/sprite.symbol.svg',
-      revision = 3;
+      revision = 5;
 
   if ( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
     return true;
